d37a6805987d44a5a343da48dbdbc16dced67a2f
LexilesR <lexilesrivas@gmail.com>
2025-06-01 23:44:27 -0400

    paciente

--- fuente/scripts/rutas/ruta.paciente.js
+++ fuente/scripts/rutas/ruta.paciente.js
@@ -5,11 +5,23 @@ const paciente = require('../model/model.paciente');
 
 router.get('/pacientes', (req, res) => {
     paciente.ver_paciente().then(pacientes => {
-        res.render('pacientes.ejs');
+        res.render('pacientes.ejs', { pacientes: pacientes });
     })
     .catch(error => {
         console.error('Error al obtener los pacientes:', error);
         res.status(500).send('Error al obtener los pacientes');
+    });
+});
+
+router.get('/buscar-pacientes', (req, res) => {
+    const filtro = req.query.q || '';
+    const patologia = req.query.patologia || '';
+    paciente.buscar_paciente(filtro, patologia).then(pacientes => {
+        res.json(pacientes);
+    })
+    .catch(error => {
+        console.error('Error al buscar pacientes:', error);
+        res.status(500).send('Error al buscar pacientes');
     });
 });
 
