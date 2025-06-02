c76bd1f62430bc71bc787c6ff2c863a1bc470a66
Dev-TonyM <unerg.tony@gmail.com>
2025-06-01 21:38:17 -0400

    no

--- fuente/scripts/rutas/ruta.register.js
+++ fuente/scripts/rutas/ruta.register.js
@@ -8,17 +8,17 @@ router.get('/register', function(req, res) {
     res.render('register.ejs', { error: null, title: 'Registro' });
 });
 
-router.post('/register', async function(req, res) {
+router.post('/register', function(req, res) {
 
 const {user, email, pass, confirm} = req.body;
     if(!user || !email || !pass || !confirm){
-        res.render('registro', {error: 'Todos los campos son obligatorios'});
+        res.render('register.ejs', {error: 'Todos los campos son obligatorios'});
     }else if(pass.length < 8){
-        res.render('registro', {error: 'La contraseña debe ser mayor a 8 caracteres'});
+        res.render('register.ejs', {error: 'La contraseña debe ser mayor a 8 caracteres'});
     }else if(modelo.existe(email) > [0]){
-        res.render('registro', {error: 'El correo ya está en uso'});
+        res.render('register.ejs', {error: 'El correo ya está en uso'});
     }else if(modelo.existe(user) > [0]){
-        res.render('registro', {error: 'El usuario ya está en uso'});
+        res.render('register.ejs', {error: 'El usuario ya está en uso'});
     }else{
     modelo.insertar(user, email, pass) 
         .then(() => {
