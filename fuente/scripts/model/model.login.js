4f4f9e461c470b56ef1575c227aa12f39bebddb2
Dev-TonyM <unerg.tony@gmail.com>
2025-06-01 22:22:37 -0400

    register funcionando

--- fuente/scripts/model/model.login.js
+++ fuente/scripts/model/model.login.js
@@ -2,10 +2,10 @@ const { resolveInclude } = require("ejs");
 const conexion = require("../db");
 const bcryptjs = require('bcryptjs');
 module.exports = {
-    existe(user,pass){conexion.query(`select usuarios.Username,usuarios.password from usuarios where usuarios.Username = ? and usuarios.password = ?`),
+    existe(user,pass){conexion.query(`select usuarios.Username,usuarios.Password from usuarios where usuarios.Username = ? and usuarios.Password = ?`),
     [user,pass], (error, resultados)=>{ 
       if(resultados.length == 0 || resultados[0].password != pass){
-        reject('Usuario o contraseña incorrectos');
+        reject('Usuario o Contraseña incorrectos');
       }else{
         resolve(resultados);
       }
