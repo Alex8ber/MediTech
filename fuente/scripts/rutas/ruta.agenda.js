366ca64243725e4d675651ddec89dcbb7757eb8a
LexilesR <lexilesrivas@gmail.com>
2025-06-05 11:39:44 -0400

    zzzz

--- fuente/scripts/rutas/ruta.agenda.js
+++ fuente/scripts/rutas/ruta.agenda.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/agenda', (req, res) => {
+router.get('/agenda', function (req, res) {
     res.render('agenda.ejs')
 })
 
+router.get('/citas' , function (req, res) {
+    res.render('citas.ejs')
+})
 
 module.exports = router
