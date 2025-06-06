d43ba6ea1ecb7f1aee6498922b5f4146a10ccc80
Dev-TonyM <unerg.tony@gmail.com>
2025-06-06 18:58:36 -0400

    personal arreglado

--- fuente/scripts/model/model.personal.js
+++ fuente/scripts/model/model.personal.js
@@ -12,8 +12,7 @@ const personal = {
                     Usuario.Email AS Email
                 FROM Personal
                 LEFT JOIN Especialidad ON Personal.Especialidad_ID = Especialidad.Id
-                LEFT JOIN Usuario ON Personal.Usuario_ID = Usuario.Id
-                WHERE Usuario.Tipo_usuario_ID = 2`,
+                LEFT JOIN Usuario ON Personal.Usuario_ID = Usuario.Id`,
                 (error, results) => {
                     if (error) {
                         return reject(error);
@@ -37,13 +36,12 @@ const personal = {
                 FROM Personal
                 LEFT JOIN Especialidad ON Personal.Especialidad_ID = Especialidad.Id
                 LEFT JOIN Usuario ON Personal.Usuario_ID = Usuario.Id
-                WHERE Usuario.Tipo_usuario_ID = 2
-                  AND (
+                WHERE(
                     Personal.Nombres LIKE ? OR 
                     Personal.Apellidos LIKE ? OR 
                     Especialidad.Descripcion LIKE ? OR 
                     Usuario.Email LIKE ?
-                  )`;
+                )`;
             const values = [`%${filtro}%`, `%${filtro}%`, `%${filtro}%`, `%${filtro}%`];
             conexion.query(query, values, (error, results) => {
                 if (error) {
